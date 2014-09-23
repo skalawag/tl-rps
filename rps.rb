00ea25bb02df65096d21186ae6c0a5c651ffda1b
Mark Scala <markscala@gmail.com>
2014-09-23 16:13:43 -0500

    added score keeping and multiple rounds

--- rps.rb
+++ rps.rb
@@ -12,27 +12,48 @@ def get_player_move
   p
 end
 
-# get the player's move
-pc = player_choice = get_player_move.upcase
+bot_score = 0
+player_score = 0
 
-# get the bot's move
-rng = Random.new
-bc = bot_choice = ["P", "R", "S"][rng.rand(0..2)]
+while true
+  # get the player's move
+  pc = player_choice = get_player_move.upcase
 
-# adjudicate
-case
-when pc == bc
-  puts "It's a tie! #{pc} versus #{bc}"
-when pc == "P" && bc == "R"
-  puts "Paper covers Rock! You win."
-when pc == "P" && bc == "S"
-  puts "Scissor cuts paper! I win."
-when pc == "R" && bc == "S"
-  puts "Rock breaks scissors! You win."
-when pc == "R" && bc == "P"
-  puts "Paper covers rock! I win."
-when pc == "S" && bc == "P"
-  puts "Scissors cuts paper! I win."
-when pc == "S" && bc == "R"
-  puts "Rock breaks scissors! I win."
+  # get the bot's move
+  rng = Random.new
+  bc = bot_choice = ["P", "R", "S"][rng.rand(0..2)]
+
+  # adjudicate
+  case
+  when pc == bc
+    puts "It's a tie! #{pc} versus #{bc}"
+  when pc == "P" && bc == "R"
+    puts "Paper covers Rock! You win."
+    player_score += 1
+  when pc == "P" && bc == "S"
+    puts "Scissor cuts paper! I win."
+    bot_score += 1
+  when pc == "R" && bc == "S"
+    puts "Rock breaks scissors! You win."
+    player_score += 1
+  when pc == "R" && bc == "P"
+    puts "Paper covers rock! I win."
+    bot_score += 1
+  when pc == "S" && bc == "P"
+    puts "Scissors cuts paper! You win."
+    player_score += 1
+  when pc == "S" && bc == "R"
+    puts "Rock breaks scissors! I win."
+    bot_score += 1
+  end
+  puts ""
+  puts "Player: #{player_score}  -  Bot: #{bot_score}"
+  puts ""
+  puts "Play again? (y/n)"
+  ans = gets.chomp
+  if ans == 'y'
+    next
+  else
+    break
+  end
 end
